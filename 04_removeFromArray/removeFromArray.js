3114290a771b43f947312bb59addb69659d4d81b
lililok <eshanu.sofia@yandex.ru>
2024-03-30 13:01:22 +0300

    04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+    for (let i = 0; i < array.length; i++) {
+        if (nums.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
