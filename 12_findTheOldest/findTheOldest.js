be3bef94c6495a105d4708de0b282e84b499c697
lililok <eshanu.sofia@yandex.ru>
2024-04-06 19:34:08 +0300

    12 find the oldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const now = new Date();
+    const year = now.getFullYear();
+    const age = array.reduce((old, curr) => {
+        const oldDeath = old.yearOfDeath !== undefined ? old.yearOfDeath : year;
+        const currDeath = curr.yearOfDeath !== undefined ? curr.yearOfDeath : year;
 
+        return oldDeath-old.yearOfBirth < currDeath-curr.yearOfBirth ? curr: old
+    })
+    return age;
 };
 
 // Do not edit below this line
