5c590f2d402af844cff0338133c9815c5c9bdf1c
lililok <eshanu.sofia@yandex.ru>
2024-04-06 17:32:36 +0300

    09 palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.replace(/[^\w\s]/g, '');
+    cleanString = cleanString.toLowerCase();
+    cleanString = cleanString.replace(/ /g, '');
+    
+    let reversedString = cleanString.split('').reduce((reversed, char) => char + reversed, '');
+    if (reversedString == cleanString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
