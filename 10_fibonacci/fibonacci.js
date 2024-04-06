32b2aa46960c31d371e9081bb1e6279749a46edd
lililok <eshanu.sofia@yandex.ru>
2024-04-06 17:41:53 +0300

    10 fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = parseInt(n);
+    if (num==1 || num==0) {
+        return num;
+    } else if (num<0) {
+        return "OOPS";
+    }
+    return num = fibonacci(num- 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
